6eb578879b4ed02afa3a5125ccfc6caa7580fdf3
liufang-git520 <3147469145@qq.com>
2020-06-16 10:58:53 +0800

    test

---
+++ src/test/java/Test.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ * @author zbw
+ * @date - 15:52
+ */
+public class Test {
+
+
+    private static char a;
+
+    public static void main(String[] args) {
+        short s1=2;
+        s1+=1;
+        a = '张';
+        System.out.println(2<<3);
+        System.out.println(a);
+      /*  String s="hello"+9+5;
+        String s1=1+5+"hello";
+       System.out.print(s);
+       System.out.print(s1);
+        float f=1;
+        System.out.println(f);*/
+    /*  Scanner scanner =new Scanner(System.in);
+      System.out.println("请输入");
+        String s=scanner.nextLine();
+        String[] s1 = s.split(" ");
+        System.out.println(s1.length);*/
+        /*for (int i=0;i<1000;i++){
+            if (i%2==1 && i%3==2 && i%4==3 && i%5==4 && i%6==5 && i%7==0){
+                System.out.println(i);
+            }
+        }*/
+    }
+    public  void run() {
+
+    }
+}
